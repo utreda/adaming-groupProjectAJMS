4571c13f4bf0c23b9e35a80fadc090d847fa198c
utreda <gautiem@gmail.com>
2019-11-19 16:25:42 +0100

    Création du projet sur intlliJ

---
+++ src/main/java/com/adaming/groupprojectajms/gestion_ects/GestionEctsApplication.java
@@ -0,0 +1,13 @@
+package com.adaming.groupprojectajms.gestion_ects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionEctsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionEctsApplication.class, args);
+    }
+
+}
