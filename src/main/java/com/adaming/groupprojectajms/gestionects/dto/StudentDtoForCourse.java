50c483518d9c4700669ed3b8d7d39b7835b20884
aad13 <adam.lounes@yahoo.fr>
2019-11-26 16:41:07 +0100

    "Addition of a new dto: StudentDtoForCourse. Subsequent modification of method getStudentsForCourse in StudentRestController"

---
+++ src/main/java/com/adaming/groupprojectajms/gestionects/dto/StudentDtoForCourse.java
@@ -0,0 +1,70 @@
+package com.adaming.groupprojectajms.gestionects.dto;
+
+import java.util.List;
+
+public class StudentDtoForCourse {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private boolean validation;
+    private boolean isAccepted;
+
+    public StudentDtoForCourse(Long id, String firstName, String lastName, String email, boolean validations, boolean isAccepted) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.validation=validations;
+        this.isAccepted = isAccepted;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isValidation() {
+        return validation;
+    }
+
+    public void setValidation(boolean validation) {
+        this.validation = validation;
+    }
+
+    public boolean isAccepted() {
+        return isAccepted;
+    }
+
+    public void setAccepted(boolean accepted) {
+        isAccepted = accepted;
+    }
+}
