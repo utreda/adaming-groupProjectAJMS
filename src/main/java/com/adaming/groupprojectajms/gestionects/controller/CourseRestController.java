ed2c23455de16fb2d01cd52b16362df8e57cc4bf
aad13 <adam.lounes@yahoo.fr>
2019-11-27 12:01:40 +0100

    "Commented all changes made to CourseRepository, CourseService and CourseRestController regarding a customize insert query to replace CrudRepository's save method. Made addNewCourse method in CourseRestController return void."

--- src/main/java/com/adaming/groupprojectajms/gestionects/controller/CourseRestController.java
+++ src/main/java/com/adaming/groupprojectajms/gestionects/controller/CourseRestController.java
@@ -33,14 +33,15 @@ public class CourseRestController {
     private StudentService studentService;
     @Autowired
     private StudentCourseService studentCourseService;
+
     @PostMapping(path = "/courses/teachers/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public List<CourseDto> addNewCourse(@RequestBody @Valid AddCourseDto courseDto, @PathVariable("id") Long id) {
+    public void addNewCourse(@RequestBody @Valid AddCourseDto courseDto, @PathVariable("id") Long id) {
         try {
+            //this.courseService.registerWithInsert(new Course(courseDto.getName(), courseDto.getEcts(), this.teacherService.fetchById(id)));
             this.courseService.register(new Course(courseDto.getName(), courseDto.getEcts(), this.teacherService.fetchById(id)));
         } catch (CourseAlreadyExistException | NullCourseException e) {
             e.printStackTrace();
         }
-        return getCoursesForTeacher(id);
     }
 
     @GetMapping(value = "/courses", produces = MediaType.APPLICATION_JSON_VALUE)
