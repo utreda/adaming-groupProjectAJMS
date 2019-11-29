ea79969af368c5b73c405c433e95ce2b67e49e00
juliagayral <ju.gayral@gmail.com>
2019-11-29 14:27:23 +0100

    test getter setter firstname, lastname and email

--- src/test/java/com/adaming/groupprojectajms/gestionects/GestionEctsApplicationTests.java
+++ src/test/java/com/adaming/groupprojectajms/gestionects/GestionEctsApplicationTests.java
@@ -1,13 +1,34 @@
 package com.adaming.groupprojectajms.gestionects;
 
+import com.adaming.groupprojectajms.gestionects.entity.Student;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class GestionEctsApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void testGetSetFirstname() {
+        Student student = new Student("Seb","Pivot","seb.pivot@live.fr");
+        student.setFirstName("Julia");
+        Assert.hasText("Julia", student.getFirstName());
     }
 
+    @Test
+    public void testGetSetLastname() {
+        Student student = new Student("Seb","Pivot","seb.pivot@live.fr");
+        student.setLastName("Gayral");
+        assertEquals("Gayral", student.getLastName());
+    }
+
+
+    @Test
+    public void testGetSetEmail() {
+        Student student = new Student("Seb","Pivot","seb.pivot@live.fr");
+        student.setEmail("ju.gayral@gmail.com");
+        assertEquals("ju.gayral@gmail.com", student.getEmail());
+    }
 }
